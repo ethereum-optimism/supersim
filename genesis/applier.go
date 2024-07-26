c49d7e73ccfca4c91b7e3fc93b369493bd6da28c
Hamdi Allam <hamdi.allam97@gmail.com>
2024-07-26 11:15:37 -0400

    genesis package

--- genesis/applier.go
+++ genesis/applier.go
@@ -1,4 +1,4 @@
-package genesisapplier
+package genesis
 
 import (
 	"encoding/json"
@@ -8,11 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 )
 
-type Config struct {
-	ChainID *big.Int
-}
-
-func UpdateGenesisWithConfig(genesisJson []byte, config Config) ([]byte, error) {
+func ApplyChainID(genesisJson []byte, chainId *big.Int) ([]byte, error) {
 	var genesis core.Genesis
 
 	err := json.Unmarshal(genesisJson, &genesis)
@@ -20,8 +16,8 @@ func UpdateGenesisWithConfig(genesisJson []byte, config Config) ([]byte, error) 
 		return nil, fmt.Errorf("unable to parse genesis json")
 	}
 
-	if config.ChainID != nil && genesis.Config.ChainID != config.ChainID {
-		genesis.Config.ChainID = config.ChainID
+	if genesis.Config.ChainID.Cmp(chainId) != 0 {
+		genesis.Config.ChainID = chainId
 	}
 
 	result, err := json.Marshal(genesis)
