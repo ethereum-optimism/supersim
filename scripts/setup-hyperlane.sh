9f2e80a7639309fc84a364709929c9dc5d19a183
nambrot <nambrot@googlemail.com>
2024-08-07 19:17:05 -0400

    Remove script

--- scripts/setup-hyperlane.sh
+++
@@ -1,6 +0,0 @@
-# hyperlane core deploy --chain l1 --yes --overrides ./hyperlane-registry
-export HYP_KEY=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
-hyperlane core deploy --chain opchaina --yes --overrides ./hyperlane-registry
-hyperlane core deploy --chain opchainb --yes --overrides ./hyperlane-registry
-
-hyperlane send
