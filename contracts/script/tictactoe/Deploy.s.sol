73835f3327d16453d033cabfa649fd9aac1a88c8
Hamdi Allam <hamdi.allam97@gmail.com>
2024-09-27 17:42:28 -0700

    TicTacToe game based played just on events

---
+++ contracts/script/tictactoe/Deploy.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {TicTacToe} from "../../src/tictactoe/TicTacToe.sol";
+
+contract DeployScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+        TicTacToe game = new TicTacToe{ salt: "tictactoe" }();
+        console.log("Deployed at: ", address(game));
+    }
+}
