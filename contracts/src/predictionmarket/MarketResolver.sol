f0c2a24a82c4e158215726d7c272e9711173af2e
Hamdi Allam <hamdi.allam97@gmail.com>
2024-10-30 15:44:56 -0400

    split the factories from the resolvers

--- contracts/src/predictionmarket/MarketResolver.sol
+++ contracts/src/predictionmarket/MarketResolver.sol
@@ -9,7 +9,7 @@ enum MarketOutcome {
 }
 
 interface IMarketResolver {
-
-    // @notice get the outcome of the market. This MUST be deterministic.
+    // @notice get the outcome of the market. This MUST be deterministic as the prediction market
+    //         will use this value to determine the outcome of the market ONCE when resolved.
     function outcome() external returns (MarketOutcome);
 }
---
+++ contracts/src/predictionmarket/factories/TicTacToePredictionMarketFactory.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {Predeploys} from "@contracts-bedrock/libraries/Predeploys.sol";
+import {ICrossL2Inbox} from "@contracts-bedrock/L2/interfaces/ICrossL2Inbox.sol";
+
+import { IMarketResolver, MarketOutcome } from "../MarketResolver.sol";
+import { PredictionMarket } from "../PredictionMarket.sol";
+import { TicTacToeGameResolver } from "../resolvers/TicTacToeResolver.sol";
+
+import { TicTacToe } from "../../tictactoe/TicTacToe.sol";
+
+contract TicTacToePredictionMarketFactory {
+    // @notice TicTacToe contract address
+    TicTacToe public game;
+
+    // @notice PredictionMarket contract address
+    PredictionMarket public predictionMarket;
+
+    // @notice Emitted when a new market for tictactoe is created
+    event TicTacToeMarketCreated(IMarketResolver resolver);
+
+    // @notice create a new factory instantiating prediction markets based
+    //         on the outcome of the TicTacToe games
+    // @param _gameAddress TicTacToe contract address
+    // @param _predictionMarket PredictionMarket contract address
+    constructor(TicTacToe _game, PredictionMarket _predictionMarket) {
+        game = _game;
+        predictionMarket = _predictionMarket;
+    }
+
+
+    // @notice create a new market for an accepted TicTacToe game. The game creator being the
+    //         yes outcome and the opponent being the no outcome.
+    function newMarket(ICrossL2Inbox.Identifier calldata _id, bytes calldata _data) external payable {
+        require(_id.origin == address(game));
+        ICrossL2Inbox(Predeploys.CROSS_L2_INBOX).validateMessage(_id, keccak256(_data));
+
+        // Ensure this is an accepted game event
+        bytes32 selector = abi.decode(_data[:32], (bytes32));
+        require(selector == TicTacToe.AcceptedGame.selector);
+
+        // Decode game identifying fields
+        (uint256 chainId, uint256 gameId, address opponent,) =
+            abi.decode(_data[32:], (uint256, uint256, address, address));
+
+        // Create resolver for the game & market
+        IMarketResolver resolver = new TicTacToeGameResolver(game, chainId, gameId, opponent);
+        predictionMarket.newMarket(resolver);
+        
+        emit TicTacToeMarketCreated(resolver);
+    }
+}
