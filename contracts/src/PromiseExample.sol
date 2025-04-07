0918f28ddd3271fb12aa6657a8b7789998e06af5
Hamdi Allam <hamdi.allam97@gmail.com>
2025-04-07 17:32:29 +0200

    remove Promise.sol in favor of interop-lib

--- contracts/src/PromiseExample.sol
+++ contracts/src/PromiseExample.sol
@@ -3,18 +3,19 @@ pragma solidity 0.8.25;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {Promise} from "./Promise.sol";
+import {Promise} from "@interop-lib/Promise.sol";
+import {PredeployAddresses} from "@interop-lib/libraries/PredeployAddresses.sol";
+
 import {Predeploys} from "@contracts-bedrock/libraries/Predeploys.sol";
 
 contract PromiseExample {
-    Promise public p = Promise(0xFcdC08d2DFf80DCDf1e954c4759B3316BdE86464);
+    Promise public p = Promise(PredeployAddresses.PROMISE);
 
     mapping(address => uint256) public wethBalances;
 
     function refreshBalance(uint256 _destination, address _account) public {
         bytes32 msgHash = p.sendMessage(_destination, Predeploys.WETH, abi.encodeWithSelector(IERC20.balanceOf.selector, _account));
         p.then(msgHash, this.update.selector, abi.encode(_account));
-
     }
 
     function update(uint256 _balance) external {
